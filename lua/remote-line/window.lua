c740b20b436bfa929e3cda3e67efb0f6f3b86fdc
ksaito <kitsit.39@gmail.com>
2024-08-10 11:23:30 +0900

    feat: implementation a menu window

---
+++ lua/remote-line/window.lua
@@ -0,0 +1,50 @@
+local open_remote = require("remote-line.open_remote")
+
+local M = {}
+
+-- TODO: Refactor this into a local function
+function SelectOption(buf, win)
+  local row = vim.api.nvim_win_get_cursor(win)[1]
+  local line = vim.api.nvim_buf_get_lines(buf, row - 1, row, false)[1]
+
+  if line == "1. Open remote repository for selected rows" then
+    open_remote.open()
+  elseif line == "2. fuga" then
+    print("fuga")
+  elseif line == "3. pigya" then
+    print("pigya")
+  end
+
+  vim.api.nvim_win_close(win, true)
+end
+
+function M.menu()
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local content = { "1. Open remote repository for selected rows", "2. fuga", "3. pigya" }
+  local width = 100
+  local height = #content
+  local opts = {
+    relative = "cursor",
+    row = 1,
+    col = col,
+    width = width,
+    height = height,
+    style = "minimal",
+    border = "single",
+  }
+
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, content)
+  local win = vim.api.nvim_open_win(buf, true, opts)
+
+  vim.api.nvim_buf_set_keymap(buf, "n", "q", ":q<CR>", { noremap = true, silent = true })
+  vim.api.nvim_buf_set_keymap(
+    buf,
+    "n",
+    "<CR>",
+    [[<cmd>lua SelectOption(]] .. buf .. [[, ]] .. win .. [[)<CR>]],
+    { noremap = true, silent = true }
+  )
+end
+
+return M
--- plugin/remote-line.lua
+++ plugin/remote-line.lua
@@ -2,4 +2,5 @@ if vim.g.loaded_remote_line then
   return
 end
 vim.g.loaded_remote_line = 1
-vim.api.nvim_command("command! -nargs=0 OpenRemote lua require('remote-line.open_remote').open()")
+
+vim.api.nvim_command("command! -nargs=0 RLmenu lua require('remote-line.window').menu()")
