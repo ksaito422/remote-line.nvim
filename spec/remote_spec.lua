f15793ba58d3ca4cd188997d41464055c5e7adfc
ksaito <kitsit.39@gmail.com>
2024-09-30 20:56:04 +0900

    test: add test for remote.copy

--- spec/remote_spec.lua
+++ spec/remote_spec.lua
@@ -64,3 +64,65 @@ describe("open", function()
     mock.revert(generate_mock)
   end)
 end)
+
+describe("copy", function()
+  local M
+  local generate_mock
+  local print_exec = spy.on(_G, "print")
+
+  before_each(function()
+    M = require("remote-line.remote")
+    generate_mock = mock(require("remote-line.generate"), true)
+
+    stub(vim.fn, "has")
+    stub(vim.fn, "system")
+  end)
+
+  it('should call "pbcopy" on macunix', function()
+    vim.fn.has.on_call_with("macunix").returns(1)
+    vim.fn.has.on_call_with("unix").returns(0)
+    vim.fn.has.on_call_with("win32").returns(0)
+    generate_mock.url.returns("http://example.com")
+
+    M.copy(1, 10, "path/example", "blob")
+
+    assert.stub(vim.fn.system).was_called_with("echo 'http://example.com' | pbcopy")
+  end)
+
+  it('should call "xclip" on unix', function()
+    vim.fn.has.on_call_with("macunix").returns(0)
+    vim.fn.has.on_call_with("unix").returns(1)
+    vim.fn.has.on_call_with("win32").returns(0)
+    generate_mock.url.returns("http://example.com")
+
+    M.copy(1, 10, "path/example", "blob")
+
+    assert.stub(vim.fn.system).was_called_with("echo 'http://example.com' | xclip -selection clipboard")
+  end)
+
+  it('should call "clip" on win32', function()
+    vim.fn.has.on_call_with("macunix").returns(0)
+    vim.fn.has.on_call_with("unix").returns(0)
+    vim.fn.has.on_call_with("win32").returns(1)
+    generate_mock.url.returns("http://example.com")
+
+    M.copy(1, 10, "path/example", "blob")
+
+    assert.stub(vim.fn.system).was_called_with("echo 'http://example.com' | clip")
+  end)
+
+  it('should print "Unsupported OS" for unknown OS', function()
+    vim.fn.has.on_call_with("macunix").returns(0)
+    vim.fn.has.on_call_with("unix").returns(0)
+    vim.fn.has.on_call_with("win32").returns(0)
+    generate_mock.url.returns("http://example.com")
+
+    M.copy(1, 10, "path/example", "blob")
+
+    assert.spy(print_exec).was_called_with("Unsupported OS")
+  end)
+
+  after_each(function()
+    mock.revert(generate_mock)
+  end)
+end)
